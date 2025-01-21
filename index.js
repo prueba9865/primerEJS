64fc4c057b7a7476f35f51dc2ffce96f058e8eb4
prueba9865 <albertoucendo88@hotmail.com>
2025-01-21 17:27:18 +0100

    segundo commit

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ app.get('/', function (req, res) {
 })
 
 app.get('/saludo', function (req, res) {
-  res.render('saludo',{hora:16});
+  res.render('saludo',{hora:"00:00"});
 })
+
+app.get('/producto', function (req, res) {
+    const datos = [
+        {id: 1, nombre: "Pepe", precio: 20, colores: ['rojo', 'negro', 'verde']},
+        {id: 2, nombre: "Antonia", precio: 50, colores: ['amarillo', 'azul', 'blanco']},
+        {id: 3, nombre: "Laura", precio: 35, colores: ['violeta', 'purpura', 'rosa']}
+      /*{color: 'rojo'},
+      {color: 'negro'},
+      {color: 'verde'}*/
+    ]
+    res.render('producto',{datos:datos});
+  })
 
 app.listen(3000)
